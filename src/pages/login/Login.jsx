012bb8a067f33700d4e9af9ea5a44863c014d566
Aliullah47 <alishafaie47@gmail.com>
2023-02-27 17:50:57 +0430

    form submited

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import styles from "./login.module.css";
+
 export default function Login() {
-  return <div>Login fsd sfd fsd fsd fsd fsd dfs fds dsf f dsf </div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h3>Login</h3>
+      <label>
+        <span>Email</span>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        <span>password</span>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 }
