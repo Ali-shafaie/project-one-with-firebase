21962c4c17f96d7b77b2acef7d350d816045554f
Aliullah47 <alishafaie47@gmail.com>
2023-02-28 10:21:10 +0430

    signup changes

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,4 +1,51 @@
-// import styles from "./Home.modul.css";
+import { useState } from "react";
+import styles from "./Signup.module.css";
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <div className={styles["signup-form"]}>
+      <h2>Sign up</h2>
+      <form action="" onSubmit={onSubmit}>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            name=""
+            onChange={(e) => setEmail(e.target.value)}
+            id=""
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            name=""
+            onChange={(e) => setPassword(e.target.value)}
+            id=""
+            value={password}
+          />
+        </label>
+        <label>
+          <span>Display Name</span>
+          <input
+            type="text"
+            name=""
+            onChange={(e) => setDisplayName(e.target.value)}
+            id=""
+            value={displayName}
+          />
+        </label>
+        <button className="btn">Login</button>
+      </form>
+    </div>
+  );
 }
