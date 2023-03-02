1a3bea1a18e3660e385c8639628a744df6b691f4
Aliullah47 <alishafaie47@gmail.com>
2023-03-02 12:25:40 +0430

    navigation add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 // pages & components
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
-
+import { useAuthContext } from "./hooks/useAuthContext";
 function App() {
+  const { authIsReady, user } = useAuthContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  }, [user]);
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
+      {authIsReady && (
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
 
-          <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
 
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
